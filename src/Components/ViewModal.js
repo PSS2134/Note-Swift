af70d7f28ee1b603cb8e5e585814341c66def4a8
Priyansh S Shrivastav <priyanshs18171819@gmail.com>
2024-01-06 04:57:31 +0530

    Notes App Updated Footer

--- src/Components/ViewModal.js
+++ src/Components/ViewModal.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import cross from "../Images/cross.png";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllNotes } from "../noteslice";
@@ -6,21 +6,24 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 
 const ViewModal = ({ closeViewModal, id }) => {
-    useEffect(()=>{
-        document.body.style.overflowY="hidden";
-        return ()=>{}
-    },[])
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {};
+  }, []);
   const notes = useSelector(getAllNotes);
   let tempEditNote = notes.filter((note) => note.Id === id);
 
-//   console.log(id);
+  //   console.log(id);
 
   return (
     <div className="flex justify-center items-center fixed top-0 right-0 left-0 backdrop-blur-sm h-screen">
       <div className="h-[90vh] w-[60%] p-[5vh] m-5 bg-slate-100 rounded-md shadow-md shadow-black backdrop-blur-3xl">
         <img
           src={cross}
-          onClick={()=>{closeViewModal(); document.body.style.overflowY="scroll";}}
+          onClick={() => {
+            closeViewModal();
+            document.body.style.overflowY = "scroll";
+          }}
           className="float-right block mb-1 cursor-pointer hover:bg-slate-300 hover:scale-105"
         />
         <h1 className="text-center m-3 text-4xl font-semibold text-slate-800 break-words">
